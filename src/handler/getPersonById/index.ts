589688f69da9b5c9469e979a22ee2e8905afc083
Roney Rogério De Oliveira <roneyrogerio92@gmail.com>
2022-09-24 07:20:49 -0300

    feat: Take Path ID in a simplified way.

--- src/handler/getPersonById/index.ts
+++ src/handler/getPersonById/index.ts
@@ -3,7 +3,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { getPersonById } from '../../services/person';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const id = event.pathParameters && event.pathParameters.id ? event.pathParameters.id : '0';
+  const id = String(event.pathParameters?.id);
   const res = await getPersonById(id);
 
   if (res.data) {
